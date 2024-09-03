34c7ffa1e6b84a1670dea12f83858d6c80fa59f3
Oliver <oliveraarnikoivu@icloud.com>
2024-09-03 09:42:05 +0200

    add other alignment options

--- src/components/editor/align.tsx
+++ src/components/editor/align.tsx
@@ -1,54 +1,93 @@
-import { AlignCenterHorizontal, AlignCenterVertical } from "lucide-react";
+import {
+  AlignCenterHorizontal,
+  AlignCenterVertical,
+  AlignEndHorizontal,
+  AlignEndVertical,
+  AlignStartHorizontal,
+  AlignStartVertical,
+} from "lucide-react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group.tsx";
 import { useComponentActions, useSelected } from "@/stores";
 import Section from "@/components/editor/section.tsx";
 import { alignCenterHorizontal, alignCenterVertical } from "@/utils/align.ts";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 const AlignOptions = {
-  "align-center-horizontal": AlignCenterHorizontal,
-  "align-center-vertical": AlignCenterVertical,
+  "align-center-horizontal": {
+    icon: AlignCenterHorizontal,
+    handler: alignCenterHorizontal,
+    tooltip: "Align center horizontally",
+  },
+  "align-center-vertical": {
+    icon: AlignCenterVertical,
+    handler: alignCenterVertical,
+    tooltip: "Align center vertically",
+  },
+  "align-end-horizontal": {
+    icon: AlignEndHorizontal,
+    handler: null,
+    tooltip: "Align end horizontally",
+  },
+  "align-end-vertical": {
+    icon: AlignEndVertical,
+    handler: null,
+    tooltip: "Align end vertically",
+  },
+  "align-start-horizontal": {
+    icon: AlignStartHorizontal,
+    handler: null,
+    tooltip: "Align start horizontally",
+  },
+  "align-start-vertical": {
+    icon: AlignStartVertical,
+    handler: null,
+    tooltip: "Align start vertically",
+  },
 };
 
 export default function Align() {
   const selectedComponents = useSelected();
   const { updateCoordinates } = useComponentActions();
 
-  const handleAlign = (key: keyof typeof AlignOptions) => {
-    switch (key) {
-      case "align-center-horizontal": {
-        const alignedComponents = alignCenterHorizontal(selectedComponents);
-        alignedComponents.forEach((c) => {
-          updateCoordinates(c.id, { x: c.coordinates.x, y: c.coordinates.y });
-        });
-        break;
-      }
-      case "align-center-vertical": {
-        const alignedComponents = alignCenterVertical(selectedComponents);
-        alignedComponents.forEach((c) => {
-          updateCoordinates(c.id, { x: c.coordinates.x, y: c.coordinates.y });
-        });
-        break;
-      }
-      default:
-        break;
-    }
+  const handleAlign = (
+    value: (typeof AlignOptions)[keyof typeof AlignOptions]
+  ) => {
+    if (!value.handler) return;
+    const alignedComponents = value.handler(selectedComponents);
+    alignedComponents.forEach((c) => {
+      updateCoordinates(c.id, {
+        x: c.coordinates.x,
+        y: c.coordinates.y,
+      });
+    });
   };
 
   if (selectedComponents.length === 1) return null;
 
   return (
     <Section title="Align">
-      <ToggleGroup type="single" className="justify-start">
-        {Object.entries(AlignOptions).map(([key, Icon]) => (
-          <ToggleGroupItem
-            key={key}
-            value={key}
-            aria-label={key}
-            onClick={() => handleAlign(key as keyof typeof AlignOptions)}
-          >
-            <Icon className="size-4" />
-          </ToggleGroupItem>
-        ))}
+      <ToggleGroup type="single" className="justify-start flex-wrap">
+        <TooltipProvider>
+          {Object.entries(AlignOptions).map(([key, value]) => (
+            <Tooltip key={key}>
+              <TooltipTrigger asChild>
+                <ToggleGroupItem
+                  value={key}
+                  aria-label={key}
+                  onClick={() => handleAlign(value)}
+                >
+                  <value.icon className="size-4" />
+                </ToggleGroupItem>
+              </TooltipTrigger>
+              <TooltipContent>{value.tooltip}</TooltipContent>
+            </Tooltip>
+          ))}
+        </TooltipProvider>
       </ToggleGroup>
     </Section>
   );
